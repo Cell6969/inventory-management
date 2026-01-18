46f83927467414a5c6d7c6fb6b81db71437a5fa8
Cell6969 <bossmarinoo@gmail.com>
2026-01-18 14:43:43 +0700

    update: customize login form

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AdminController extends Controller
 {
     public function destroy(Request $request)
     {
+
+        $user = Auth::user();
+        Log::info($user);
 
         Auth::guard('web')->logout();
 
